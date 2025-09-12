26cf38bbbb89518e40791a8a3d527c00a206675d
Aikya Halder <aikyahd@gmail.com>
2025-09-13 02:04:47 +0600

    New icons added

--- includes/class/class-proler-product-handler.php
+++ includes/class/class-proler-product-handler.php
@@ -85,6 +85,7 @@ if ( ! class_exists( 'Proler_Product_Handler' ) ) {
 			if( empty( $discount ) ) return;
 			?>
 			<div class="proler-saving">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="m21.5 9.757-5.278 4.354 1.649 7.389L12 17.278 6.129 21.5l1.649-7.389L2.5 9.757l6.333-.924L12 2.5l3.167 6.333z"/></svg>
 				<?php
 					echo sprintf(
 						// translators: %1$s: maximum discount volume, %2$s: discount type, either percent or amount.
